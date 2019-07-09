18e68d511d406b38b6fde42591a3771525a6430d
nataliescuestaa23 <cuestanatalie@gmail.com>
2019-07-09 13:19:24 -0700

    Initial Comment

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var roleModelsOfNatalie = [String]()
+roleModelsOfNatalie("My sister" , "Alex Morgan", "Alexandria Ocasio Cortez")
+//print(RoleModelsOfNatalie[2])
+roleModelsOfNatalie.append("Sky Brown"[3]
+)
+
+//I am declaring a dictionary
+var recipe = ["almond flour":"3 and 1/3 cups","gluten-free oats": "2 cups", "mini chocolate chips": "1 cup"]
+
+//key/value pairs
+//keys: almond, gluten-free oats, and mini chocolate chips
+//values: 3 and 1/3 cups, 2 cups, 1 cup
+
+
+//declare a dictionary with out knowing data
+var bread :[String; +String;] = [:]
+print(bread)
+//adding the key of flour and assigning it a value
+bread["almond flour"] = "3 and 1/2 cups"
+bread[gluten-free oats] = "2 cups"
+print(bread)
+print(perfectTen["almond flour"])
+
+print(perfectTen.keys)
+//->["almond flour"] , "gluten-free oats" , "mini chocolate chips"
+//!get rid of the Optional if WE KNOW there is information stored
+print(bread["almond flour"])
+print(bread.keys)
+print(bread.values)
+
+
+
